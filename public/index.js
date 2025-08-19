e4a3ff8e442dc4acb51ca8a0e06d137c67a410d4
Maksym Koval <srcmaxim@gmail.com>
2025-08-19 15:37:48 +0200

    Fix import for preload.js

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 import { registerCodeViewerComponent } from "./components/code-viewer/code-viewer.js";
 import { registerTabPanelComponent } from "./components/tab-panel/tab-panel.js";
 import { registerBlogLatestPosts } from "./blog/components/blog-latest-posts.js";
-import { registerPreload } from "../components/preload/preload.js";
+import { registerPreload } from "./components/preload/preload.js";
 
 const app = async () => {
     registerBlogLatestPosts();
